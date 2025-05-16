38bda5fd5c84d7602d44d239b8c0f7df234d06d1
Jonas <jonasoliveira11pt@gmail.com>
2025-05-16 20:12:38 -0300

    Exercicios Pratica01

---
+++ pratica01/02.py
@@ -0,0 +1,4 @@
+numero1 = 12
+numero2 = 14
+
+print(f"Adição: {numero1} + {numero2} = {numero1 + numero2}")
---
+++ pratica01/04.py
@@ -0,0 +1,4 @@
+Preco_unitario = 12.40
+Quantidade = 3
+
+print(f"Nome_do_produto: {Preco_unitario} * {Quantidade} = {Preco_unitario * Quantidade}")
